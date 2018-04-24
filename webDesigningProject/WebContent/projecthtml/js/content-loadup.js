ce4a17f7b6a7cfcf703110f0be1dfbc15888fba7
Bernardo Sze <bernardo.sze@gmail.com>
2018-04-24 17:22:46 -0400

    B. Sze: Pages updates

--- webDesigningProject/WebContent/projecthtml/js/content-loadup.js
+++ webDesigningProject/WebContent/projecthtml/js/content-loadup.js
@@ -1,4 +1,13 @@
 $(window).load(function () {
    $("header").load("headerproject.html");
+
    $("footer").load("footer_project.html");
+
+   $("#tabs").tabs();
+
+   $("#accordion").accordion({
+      event: "click",
+      heightStyle: "content",
+      collapsible: true
+   });
 });
